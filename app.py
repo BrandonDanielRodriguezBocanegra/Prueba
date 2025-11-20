3f2a425af4e08649412a11e7ec736686df39d96d
BrandonDanielRodriguezBocanegra <adustgore5@gmail.com>
2025-11-19 22:07:12 -0600

    aCT

--- app.py
+++ app.py
@@ -345,3 +345,7 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() in ('1', 'true')
     app.run(host='0.0.0.0', port=port, debug=debug_mode)
+
+
+
+# ----------------------- fin -----------------------

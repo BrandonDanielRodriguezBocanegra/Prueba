678cd7add702eb684026242e1e519e14d81f6be0
BrandonDanielRodriguezBocanegra <adustgore5@gmail.com>
2026-01-16 15:21:57 -0600

    prov

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ import psycopg.errors
 
 import boto3
 
+MONTHS = {
+    1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril",
+    5: "Mayo", 6: "Junio", 7: "Julio", 8: "Agosto",
+    9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"
+}
+
+
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "supersecretkey")
 
